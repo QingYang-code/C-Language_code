b93335f65fe4f139e45962cbc2cc4b51f0f13db6
qingyang16 <13887276231@163.com>
2025-09-30 21:01:07 +0800

    test

---
+++ test_8_9/test_8_9/test.c
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//
+//int main()
+//{
+//	printf("hehe\n");
+//	return 0;
+//}
+
+
+int main()
+{
+	int score = 0;
+	printf("请输入成绩：");
+
+	scanf("%d", &score);
+
+	printf("你的成绩是：%d\n", score);
+
+
+
+	return 0;
+}
